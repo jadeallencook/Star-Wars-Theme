cdf4be3d0e0b63a5145a0f4702ada8e2e3348b05
Jade Allen Cook <jcook@deseretnews.com>
2017-12-13 11:12:05 -0700

    ⚙️ Now works with Deseret's custom CSS

--- app.js
+++ app.js
@@ -15,12 +15,22 @@
       this.animation = this.el.find('.animation');
       // start the animation on click
       this.start.click($.proxy(function () {
-        $('#star-wars-content').append($('.article-body-text-wrapper')[0].innerText);
+        // extract article data
+        for (var x = 0, max = document.querySelectorAll('.article-body-text-wrapper > p').length; x < max; x++) {
+          var text = '<p>' + document.querySelectorAll('.article-body-text-wrapper > p')[x].innerText + '</p>';
+          $('#star-wars-content').append(text);
+        }
+        // show animation
         window.scrollTo(0, 0);
         this.start.hide();
         this.el.append(this.animation);
+        // exiting animation events
         $('.starwars').click(function () {
           $('.starwars').hide();
+          $(window).off('scroll.disableScroll');
+        });
+        $(window).on('scroll.disableScroll', function() {
+          window.scrollTo(0, 0);
         });
       }, this));
     }
